5673b473e6784fc8229754fc8c1c7e45abec78a9
TimeLogic <martinade577@gmail.com>
2026-06-05 14:50:03 +0000

    Desktop: HashRouter for packaged file:// (fixes blank window); build .deb+.exe only (drop AppImage)

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
+// HashRouter (not BrowserRouter): packaged Electron loads index.html over the
+// file:// protocol, where history-based routing can't match and renders blank.
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
